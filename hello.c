891dba61509e42fbb277e6ab01e81ec39538e582
bgunia1 <bgunia@unomaha.edu>
2022-08-28 08:06:39 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Brody Gunia
+ * Date: 2022/08/23
+ *
+ * A simple hello program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Brody Gunia\n");
+
+    return 0;
+}
