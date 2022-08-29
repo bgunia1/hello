c385ac96463f4861529d33e46f9555ce4260ed0d
LatrHatr20 <latrhatr20@gmail.com>
2022-08-29 02:55:51 +0000

    added my name and major

--- hello.c
+++ hello.c
@@ -12,5 +12,7 @@ int main(int argc, char **argv) {
 
     printf("Brody Gunia!!\n");
     printf("Electrical Engineer\n");
+    printf("Jonathan Barnes\n");
+    printf("Computer Engineer\n");
     return 0;
 }
